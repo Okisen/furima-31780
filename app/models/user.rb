8815cceede01d7eeb69ebdda55fafb79e9066e7b
Okisen <sixthjojo@gmail.com>
2020-10-26 19:15:43 +0900

    バリデーションの記述の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :encrypted_password, format: { with: /[a-z]{1,}/i }
-  validates :encrypted_password, format: { with: /[0-9]{1,}/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name_ruby, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :last_name_ruby, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name_ruby, format: { with: /\A[ァ-ン]+\z/ }
+    validates :last_name_ruby, format: { with: /\A[ァ-ン]+\z/ }
+    validates :birthday
+  end
+  validates :email, uniqueness: {case_sensitive: false}
+  validates :password, format: { with: /[a-z]{1,}[0-9]{1,}/i }
+  # validates :password, format: { with: /[0-9]{1,}/ }
 end
