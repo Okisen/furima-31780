682a1e02b87853331b5d164af8d8b3a07a754deb
Okisen <sixthjojo@gmail.com>
2020-10-27 17:30:58 +0900

    itemテーブルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :state_id
+    validates :delivery_charges_id
+    validates :delivery_area_id
+    validates :days_id
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
 end
