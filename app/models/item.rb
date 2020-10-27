435c05105f2e20890d5973f1115ca6311b4218c1
Okisen <sixthjojo@gmail.com>
2020-10-27 19:37:25 +0900

    itemテーブルのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :state
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :delivery_area
+  belongs_to_active_hash :day
   belongs_to :user
   has_one_attached :image
 
@@ -6,11 +12,11 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :description
-    validates :category_id
-    validates :state_id
-    validates :delivery_charge_id
+    validates :category_id,         numericality: {other_than: 1}
+    validates :state_id,            numericality: {other_than: 1}
+    validates :delivery_charge_id,  numericality: {other_than: 1}
     validates :delivery_area_id
-    validates :day_id
+    validates :day_id,              numericality: {other_than: 1}
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 end
