e406c58d659d55b09cc4fe2923397e35d82b9e98
Okisen <sixthjojo@gmail.com>
2020-10-28 20:05:12 +0900

    商品購入画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, :new, :create, :show, :destroy]
+  resources :items, only: [:index, :new, :create, :show, :destroy] do
+    resources :trades, only: :index
+  end
 end
