27d772d5224fde56a97dde4f650086bb838cd04c
Okisen <sixthjojo@gmail.com>
2020-10-28 14:46:41 +0900

    テストコード 仮置き

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    name                  {Faker::Name.name}
+    description           {Faker::Lorem.sentence}
+    category_id           {Faker::Number.between(from: 2, to: 11)}
+    state_id              {Faker::Number.between(from: 2, to: 7)}
+    delivery_charge_id    {Faker::Number.between(from: 2, to: 3)}
+    delivery_area_id      {Faker::Number.between(from: 1, to: 47)}
+    day_id                {Faker::Number.between(from: 2, to: 4)}
+    price                 {Faker::Number.between(from: 300, to: 9999999)}
+
+    after(:build) do |test|
+      test.image.attach(io: File.open('app/assets/images/camera.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it 'image,name,description,category,state_id,delivery_charge_id,delivery_area_id,day_id,priceが存在すれば保存できる' do
+      expect(@item).to be_valid
+    end
+    it 'imageが存在しないと保存できない' do
+      @item.image = nil
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+    it 'nameが存在しないと保存できない' do
+      @item.name = nil
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+    it 'descriptionが存在しないと保存できない' do
+      @item.description = nil
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+    it 'categoryが存在しないと保存できない' do
+      @item.category = nil
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+    it 'state_idが存在しないと保存できない' do
+      @item.state_id = nil
+      expect(@item.errors.full_messages).to include("State can't be blank")
+    end
+    it 'delivery_charge_idが存在しないと保存できない' do
+      @item.delivery_charge_id = nil
+      expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+    end
+    it 'delivery_area_idが存在しないと保存できない' do
+      @item.delivery_area_id = nil
+      expect(@item.errors.full_messages).to include("Delivery area can't be blank")
+    end
+    it 'day_idが存在しないと保存できない' do
+      @item.day_id = nil
+      expect(@item.errors.full_messages).to include("Day can't be blank")
+    end
+    it 'priceが存在しないと保存できない' do
+      @item.price = nil
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+    it 'priceが300より小さいとき保存できない' do
+      
+    end
+    it 'priceが9999999より大きいとき保存できない' do
+      
+    end
+    it 'priceが全角数字のとき保存できない' do
+      
+    end
+    it 'priceが半角英字のとき保存できない' do
+      
+    end
+    it 'priceが半角数字のみのとき保存できる' do
+      
+    end
+    it 'userが紐づいていないとき保存できない' do
+      
+    end
+  end
 end
