f009a6173810b580917273d22856798c2445774f
Okisen <sixthjojo@gmail.com>
2020-10-29 09:50:57 +0900

    BuyerAddressテーブルにバリデーション

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,3 +1,10 @@
 class BuyerAddress < ApplicationRecord
+  with_options presence: true do
+    validates :postal_code, format:{ with:/\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id
+    validates :city
+    validates :address_number
+    validates :phone_number, format:{ with:/\A\d+\z/}
+  end
   belongs_to :trade
 end
