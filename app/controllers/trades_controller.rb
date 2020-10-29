cd0649c747b082d4f93424ffa7916231454c019e
Okisen <sixthjojo@gmail.com>
2020-10-29 16:50:11 +0900

    購入時各テーブルへの保存

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -7,6 +7,7 @@ class TradesController < ApplicationController
     set_item
     @item_trade = ItemTrade.new(item_trade_params)
     if @item_trade.valid?
+      pay_item
       @item_trade.save
       redirect_to root_path
     else
@@ -15,11 +16,18 @@ class TradesController < ApplicationController
   end
 
   private
-
   def set_item
     @item = Item.find(params[:item_id])
   end
   def item_trade_params
-    params.require(:item_trade).permit(:postal_code, :prefecture_id, :city, :address_number, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:item_trade).permit( :postal_code, :prefecture_id, :city, :address_number, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token], price: @item.price)
+  end
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: item_trade_params[:price],
+        card: item_trade_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/item_trade.rb
+++ app/models/item_trade.rb
@@ -1,12 +1,13 @@
 class ItemTrade
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address_number, :building, :phone_number
+  attr_accessor :token, :price, :user_id, :item_id, :postal_code, :prefecture_id, :city, :address_number, :building, :phone_number
   with_options presence: true do
     validates :postal_code, format:{ with:/\A\d{3}[-]\d{4}\z/}
     validates :prefecture_id
     validates :city
     validates :address_number
     validates :phone_number, format:{ with:/\A\d+\z/}
+    validates :token
   end
   def save
     trade = Trade.create(user_id: @user_id, item_id: @item_id)
