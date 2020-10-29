3314b2e7628ff602a5f51b2c7165765cfcec7a6e
Okisen <sixthjojo@gmail.com>
2020-10-29 14:48:14 +0900

    フォームオブジェクトの導入

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,9 +1,25 @@
 class TradesController < ApplicationController
   def index
+    @item_trade = ItemTrade.new
     set_item
   end
+  def create
+    set_item
+    @item_trade = ItemTrade.new(item_trade_params)
+    if @item_trade.valid?
+      @item_trade.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
   private
+
   def set_item
     @item = Item.find(params[:item_id])
+  end
+  def item_trade_params
+    params.require(:item_trade).permit(:postal_code, :prefecture_id, :city, :address_number, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items, only: [:index, :new, :create, :show, :destroy] do
-    resources :trades, only: :index
+    resources :trades, only: [:index, :create]
   end
 end
